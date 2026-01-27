002ae471ee0aca42573cf7a8b97fa1f5943c6b3b
Krish <your.email@example.com>
2026-01-27 19:07:17 +0530

    use env api url

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Verified Port 5000
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
 });
+
 
 api.interceptors.request.use((config) => {
   const token = Cookies.get('token');
